176de62c1e1a32f57ddfff1f27899bac364befe3
AlIbay8 <43456952+AlIbay8@users.noreply.github.com>
2018-11-29 15:47:53 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+$(".persona").click(function() {
+    $(".arsene").fadeToggle();
+});
+
+$(".batonpass").click(function() {
+    $(".batonpassgif").slideToggle();
+});
+
+$(".sendcard").click(function() {
+    $(".normal").toggle();
+    $(".sendcard").toggle();
+    $(".phantomThief").toggle();
+    $(".back").toggle();
+});
+
+$(".back").click(function() {
+    $(".normal").toggle();
+    $(".sendcard").toggle();
+    $(".phantomThief").toggle();
+    $(".back").toggle();
+});
